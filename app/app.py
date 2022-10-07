03521c310424801989ccfbe7e21d52e69d915361
Charles Yang <yarlescy@gmail.com>
2022-10-07 10:03:11 -0500

    updated landing page route(s)

--- app/app.py
+++ app/app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('landing.html')
+@app.route('/index')
+@app.route('/landing')
+@app.route('/home')
+def index():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
