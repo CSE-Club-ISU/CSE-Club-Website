ab86486b9c5c30f0be8f227395f089d2d88bfe26
Charles Yang <yarlescy@gmail.com>
2022-10-07 10:04:27 -0500

    fixed blank lines for pep8

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 @app.route('/landing')
@@ -9,21 +10,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/officers')
 def officers():
     return render_template('officers.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/sponsors')
 def sponsors():
     return render_template('sponsors.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
